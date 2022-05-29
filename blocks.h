cec40de58e6ba7141fdeabb5bb9a3c43ec384557
Blake Weissman <contact@blakeweissman.com>
2022-05-29 13:53:02 -0400

    Added custom blocks

--- blocks.h
+++ blocks.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
+	{"",	"memory",	10,	14},
+//	{"",	"cpu",		10,	18},
+//	{"",	"cpubars",		10,	18},
+	{"",	"volume",	1,	10},
+	{"",	"battery",	5,	3},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 100;
